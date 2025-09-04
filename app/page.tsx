af134cad57d56e6e1aa336970cf2c85eb01daea8
Hicham1970 <devhicham2@gmail.com>
2025-09-04 23:22:54 +0100

    add Main page with the motion features

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
+import Main from "@/components/main"
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900">
       <Navbar />
       <Hero />
+      <Main />
     </div>
   );
 }
