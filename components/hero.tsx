217a07df8c06be73f7d23b837e614652cf9a7dca
Hicham1970 <devhicham2@gmail.com>
2025-09-05 12:29:01 +0100

    add the moving particles in the hero page

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,12 +3,15 @@
 import { motion, useScroll, useTransform } from 'framer-motion'
 import React from 'react'
 import Image from 'next/image'
+import { ParticleCanvas } from '@/hooks/particle';
 
 export default function Hero() {
     const { scrollY } = useScroll()
     const y = useTransform(scrollY, [0, 500], [0, 100])
     return (
         <section className="min-h-screen relative overflow-hidden bg-black">
+            {/* Background Canvas */}
+            <ParticleCanvas />
             <div className="max-w-7xl mx-auto px-6 pt-32">
                 <div className='flex flex-col lg:flex-row items-center gap-16'>
                     {/* Text Content */}
